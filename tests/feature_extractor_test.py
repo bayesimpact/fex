64730d01d669cd3aae138a1f0a6a018a16c40744
Stephan Gabler <stephan.gabler@gmail.com>
2016-03-23 15:29:32 +0100

    switch to UnitTest framework

--- tests/feature_extractor_test.py
+++ tests/feature_extractor_test.py
@@ -1,5 +1,7 @@
 """Tests for the fex FeatureExtractor."""
 
+import unittest
+
 from fex import FeatureExtractor
 
 
@@ -10,16 +12,26 @@ COLUMN_NAME = 'test_col'
 class TestExtractor(FeatureExtractor):
     """A very simple test extractor."""
 
+    def __init__(self, row_id, col_name, value):
+        """Remember values to make tests more readable."""
+        super(TestExtractor, self).__init__()
+        self.row_id = row_id
+        self.col_name = col_name
+        self.value = value
+
     def extract(self):
         """Overriden method to emit data."""
-        self.emit(ROW_ID, COLUMN_NAME, 42)
+        self.emit(self.row_id, self.col_name, self.value)
 
 
-def column_naming_test():
-    """Column name should be a combination of FeatureExtractor and column name."""
-    test_extractor = TestExtractor()
-    test_extractor.extract()
-    row = test_extractor._data_store[ROW_ID]
-    col_name = list(row.keys())[0]
-    assert col_name.startswith(TestExtractor.__name__)
-    assert col_name.endswith(COLUMN_NAME)
+class FeatureExtractorTest(unittest.TestCase):
+    """Test feature extractor."""
+
+    def column_naming_test(self):
+        """Column name should be a combination of class- and col name."""
+        test_extractor = TestExtractor('1', 'col_1', 42)
+        test_extractor.extract()
+        row = test_extractor._data_store['1']
+        col_name = list(row.keys())[0]
+        self.assertTrue(col_name.startswith(TestExtractor.__name__))
+        self.assertTrue(col_name.endswith('col_1'))
